916963d3ef72554c17e8c1f59ea5ddf589637b37
Eric Horodyski <horodyski@ionic.io>
2020-11-16 08:51:37 -0500

    Add Dynamic Slide

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,63 @@
-import React from 'react';
-import './ExploreContainer.css';
+import {
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonSlide,
+  IonSlides,
+} from "@ionic/react";
+import React, { useEffect, useRef, useState } from "react";
 
-interface ContainerProps { }
+const SlidingSegments: React.FC = () => {
+  const [segments, setSegments] = useState<string[]>(["friends", "enemies"]);
+  const slider = useRef<HTMLIonSlidesElement>(null);
+  const [value, setValue] = useState("friends");
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
+  const handleSegmentChange = async (e: any) => {
+    const selectedSegment = segments.findIndex((el) => el === e.detail.value!);
+    setValue(e.detail.value!);
+    slider.current!.slideTo(selectedSegment);
+  };
+
+  const handleSlideChange = async (e: any) => {
+    const activeSlide = await slider.current?.getActiveIndex();
+    setValue(segments[activeSlide!]);
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const swiper = await slider.current?.getSwiper();
+      console.log(swiper);
+
+      setTimeout(() => {
+        setSegments(["fiends", "enemies", "heroes"]);
+      (swiper as any).update();
+
+      }, 2000);
+
+    };
+    init();
+  }, [slider]);
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <>
+      <IonSegment onIonChange={(e) => handleSegmentChange(e)} value={value}>
+        {segments.map((segment, idx) => (
+          <IonSegmentButton key={idx} value={segment}>
+            <IonLabel>{segment}</IonLabel>
+          </IonSegmentButton>
+        ))}
+      </IonSegment>
+      <IonSlides
+        pager={true}
+        ref={slider}
+        onIonSlideDidChange={(e) => handleSlideChange(e)}
+      >
+        {segments.map((segment, idx) => (<IonSlide key={idx}><p>{segment}</p></IonSlide>))}
+
+
+      </IonSlides>
+    </>
   );
 };
 
-export default ExploreContainer;
+export default SlidingSegments;
